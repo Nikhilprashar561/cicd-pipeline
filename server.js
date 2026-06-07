7c95b8b28e55ea2d8183222be90d17a4965f8a42
nikhilprashar561 <nikhilprashar561@gmail.com>
2026-06-08 01:07:56 +0530

    fix ci/cd error

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.get("/health", (req, res) => {
     message: "Health Check Successful"})
 })
 
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
